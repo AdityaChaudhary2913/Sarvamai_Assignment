8496a2d1175b106649ce3cda46495f271191dd27
AdityaChaudhary2913 <adityachaudhary1306@gmail.com>
2025-06-05 08:04:05 +0530

    Enviroment setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Sarvamai_Assignment",             
+    version="0.1.0",                    
+    author="Aditya Chaudhary",                   
+    author_email="adityachaudhary1306@gmail.com",       
+    description="Load Testing & Benchmarking for Sarvam Transliteration API",
+    url="https://github.com/AdityaChaudhary2913/Sarvamai_Assignment", 
+    packages=find_packages(), 
+    install_requires=[],
+    python_requires=">=3.7",
+)
